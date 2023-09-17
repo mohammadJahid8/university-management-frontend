af8e66ca3cc47308769284b299f0a057d8f33cec
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-09-17 11:22:42 +0600

    [Done] Logout user

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,14 +1,24 @@
+import { authKey } from "@/constants/storageKey";
+import { removeUserInfo } from "@/services/auth.service";
 import { UserOutlined } from "@ant-design/icons";
 import { Avatar, Button, Dropdown, Layout, MenuProps, Row, Space } from "antd";
+import { useRouter } from "next/navigation";
 
 const { Header: AntHeader } = Layout;
 
 const Header = () => {
+  const router = useRouter();
+
+  const logOut = () => {
+    removeUserInfo(authKey);
+    router.push("/login");
+  };
+
   const items: MenuProps["items"] = [
     {
       key: "0",
       label: (
-        <Button type="text" danger>
+        <Button type="text" danger onClick={logOut}>
           Logout
         </Button>
       ),
