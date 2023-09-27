2826c00e843949a08f0714714b11ed51950d3efa
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-09-27 18:47:00 +0600

    [Done] Handled dashboard by dynamic role

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -5,13 +5,14 @@ import React, { useState } from "react";
 import { Layout, Menu, theme } from "antd";
 import { SidebarItems } from "@/constants/SidebarItems";
 import { USER_ROLE } from "@/constants/role";
+import { getUserInfo } from "@/services/auth.service";
 
 const { Sider } = Layout;
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
 
-  const role = USER_ROLE.STUDENT;
+  const { role } = getUserInfo() as any;
 
   return (
     <Sider
